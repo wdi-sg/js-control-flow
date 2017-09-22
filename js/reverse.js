0be09d33c2ffbc521ef8498ac62e44cf1c1a84d0
Ray Tham <RayWTham@gmail.com>
2017-09-22 10:51:36 +0800

    added 'reverse' as alternative method for reference only

--- js/reverse.js
+++ js/reverse.js
@@ -1,5 +1,6 @@
 var inputString = 'building'
 var outputString=""
+// inputString.split('')//.reverse().join('')
 for (var i = inputString.length-1; i >= 0; i--) {
   outputString += inputString[i]
 }
