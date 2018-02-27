bbf3dabfc1a26e6f5e2e2cc2ea92908bc87a9b2e
Walter <walter.weixiang@gmail.com>
2018-02-27 14:53:34 +0800

    reverse.js done

---
+++ js/reverse.js
@@ -0,0 +1,7 @@
+var inputString = 'building';
+var outputString = '';
+for(i = inputString.length - 1;  i >= 0  ;  i = i-1)
+	{
+	outputString = outputString + inputString[i] ;
+	}
+console.log(outputString);
